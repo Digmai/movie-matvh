17e5e30f3735f7f5d7809a4bf9d47589afaddff7
Digmai <qt20130@gmail.com>
2023-06-29 00:19:36 +0300

    fix aduptive in devise searchBar btn

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,35 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-  const type = { movie: "movie", series: "series", episode: "episode" };
+  const type = ["movie", "series", "episode"];
 
   return (
-    <div className="w-full flex  bg-slate-400/40 p-8 my-4">
-      <nav className="w-3/4 bg-slate-400/70 text-slate-50 flex">
-        <Link
-          className=" rounded mx-4 p-2 bg-slate-500/80"
-          to={"/" + type.series}
-        >
-          {type.series.toLocaleUpperCase()}
-        </Link>
-        <Link
-          className="rounded mx-4 p-2 bg-slate-500/80"
-          to={"/" + type.movie}
-        >
-          {type.movie.toLocaleUpperCase()}
-        </Link>
-        <Link
-          className=" rounded mx-4 p-2  bg-slate-500/80"
-          to={"/" + type.episode}
-        >
-          {type.episode.toLocaleUpperCase()}
-        </Link>
-      </nav>
-
+    <div className="w-full flex  bg-slate-400/40 p-8 my-4 flex-col sm:flex-row-reverse ">
       <input
-        className="w-1/4 placeholder:bg-slate-400/100 mx-8 border-slate-100 border-2 text-indigo-300 bg-slate-950 "
+        className="w-full rounded sm:w-1/4 mx-auto sm:mx-8 sm:my-1  "
         placeholder="Search"
       />
+
+      <nav className="w-full  sm:w-3/4  bg-slate-400/70 text-slate-50 flex flex-col sm:flex-row-reverse my-2 sm:my-1 p-4 sm:p-0 ">
+        {type.map((e, i) => (
+          <Link
+            key={e + i}
+            className="  rounded my-1 sm:mx-4 p-2 bg-slate-500/80"
+            to={"/" + e}
+          >
+            {e.toLocaleUpperCase()}
+          </Link>
+        ))}
+      </nav>
     </div>
   );
 };
